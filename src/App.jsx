4ba4ea9c1a8d3577c83ffe9d9614cf97f7596c15
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2022-06-16 18:37:25 +0530

    uncomment assignment rendering

--- src/App.jsx
+++ src/App.jsx
@@ -47,8 +47,7 @@ class App extends React.Component {
   render() {
     let tabChoice = <div />;
 
-    /*Uncomment below to render assignments*/
-    /*if (this.state.buttonClicked === "assignments") {
+    if (this.state.buttonClicked === "assignments") {
       tabChoice = (
         <List
           placeholder="Add Assignment..."
@@ -57,7 +56,7 @@ class App extends React.Component {
           title="Assignments"
         />
       );
-    }*/
+    }
 
     /* Change below to render students*/
 
